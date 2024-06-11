2a36cebe9efc0737309a2bfb26379df3803db975
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-11 18:55:16 +0000

    Feat: update

--- src/pages/views/order/OrderPage.jsx
+++ src/pages/views/order/OrderPage.jsx
@@ -3,6 +3,7 @@ import { orderedProd, prodHistory } from '../../../utils/products'
 import { useDispatch, useSelector } from 'react-redux';
 import { loadHistory } from '../../../store/historySlice';
 import Alert from '../../../common/Alert';
+import Empty from '../../../common/Empty';
 
 const OrderPage = () => {
     const dispatch = useDispatch();
@@ -25,43 +26,47 @@ const OrderPage = () => {
             </button>
         </div>
       <div className='w-full py-[2rem] overflow-auto'>
-        <table className='min-w-[700px] w-full uncut-regular'>
-            <thead className='w-full xl:text-[1rem] text-[0.9rem] bg-[#EFEFEF]'>
-                <tr className=' text-left'>
-                    <th className='pl-[1.5rem] py-[0.8rem]'>Customer</th>
-                    <th className='py-[0.8rem]'>Product</th>
-                    <th className='py-[0.8rem]'>Qty</th>
-                    <th className='py-[0.8rem]'>Ttl Price</th>
-                    <th className='py-[0.8rem]'>Delivery</th>
-                    <th className='py-[0.8rem]'>Date</th>
-                    <th className='py-[0.8rem]'>Status</th>
-                </tr>
-            </thead>
-            <tbody className='bg-white'>
-                {
-                    historyItems.map((history, index) => (
-                        <tr key={index} className='xl:text-[1rem] lg:text-[0.9rem] text-[0.8rem] bg-white border-b-2 transition-all hover:scale-[1.02]'>
-                            <td className='py-[1rem] pl-[1.5rem]'>{history.userDetails.name}</td>
-                            <td>{history.product.productName}</td>
-                            <td>{history.quantity} x {history.price}</td>
-                            <td>{history.totalPrice} </td>
-                            <td>{history.userDetails.isDelivery ? 'Delivery' : 'Pick up'}</td>
-                            <td>10/06/24</td>
-                            <td className=' '>
-                                <div className={`w-[120px] px-[0.5rem] py-[0.3rem] flex justify-between items-center ${history.status === "Pending" ? "bg-[#FFE818]" : history.status === "Canceled" ? "bg-[#FF1818]" : "bg-[#39FF18]"} rounded-md`}>
-                                    <div className='uncut text-[0.9rem] font-semibold'>Delivered</div>
-                                    <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                                        </svg>
+        {historyItems.length > 0 ? (<>
+            <table className='min-w-[700px] w-full uncut-regular'>
+                <thead className='w-full xl:text-[1rem] text-[0.9rem] bg-[#EFEFEF]'>
+                    <tr className=' text-left'>
+                        <th className='pl-[1.5rem] py-[0.8rem]'>Customer</th>
+                        <th className='py-[0.8rem]'>Product</th>
+                        <th className='py-[0.8rem]'>Qty</th>
+                        <th className='py-[0.8rem]'>Ttl Price</th>
+                        <th className='py-[0.8rem]'>Delivery</th>
+                        <th className='py-[0.8rem]'>Date</th>
+                        <th className='py-[0.8rem]'>Status</th>
+                    </tr>
+                </thead>
+                <tbody className='bg-white'>
+                    {
+                        historyItems.map((history, index) => (
+                            <tr key={index} className='xl:text-[1rem] lg:text-[0.9rem] text-[0.8rem] bg-white border-b-2 transition-all hover:scale-[1.02]'>
+                                <td className='py-[1rem] pl-[1.5rem]'>{history.userDetails.name}</td>
+                                <td>{history.product.productName}</td>
+                                <td>{history.quantity} x {history.price}</td>
+                                <td>{history.totalPrice} </td>
+                                <td>{history.userDetails.isDelivery ? 'Delivery' : 'Pick up'}</td>
+                                <td>10/06/24</td>
+                                <td className=' '>
+                                    <div className={`w-[120px] px-[0.5rem] py-[0.3rem] flex justify-between items-center ${history.status === "Pending" ? "bg-[#FFE818]" : history.status === "Canceled" ? "bg-[#FF1818]" : "bg-[#39FF18]"} rounded-md`}>
+                                        <div className='uncut text-[0.9rem] font-semibold'>Delivered</div>
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>):(<>
+            <div className='w-full flex flex-col justify-center items-center pt-[4rem]'><Empty text={'No order available'}/></div>
+        </>)}
         <div className='w-full py-[2rem] bg-white'>
             
         </div>
--- src/pages/views/products/ProductsPage.jsx
+++ src/pages/views/products/ProductsPage.jsx
@@ -5,6 +5,8 @@ import { loadProducts } from '../../../store/productSlice';
 import Empty from '../../../common/Empty';
 import { loadCart, removeItem } from '../../../store/cartSlice';
 import Alert from '../../../common/Alert';
+import Button from '../../../common/Button';
+import { CSVLink } from 'react-csv';
 
 const ProductsPage = () => {
     const cartItems = useSelector(state => state.cart.items);
@@ -30,53 +32,59 @@ const ProductsPage = () => {
         setAlert({...isAlert, status: true, text: "Product deleted!", type: 'error'})
       };
 
+
+
   return (
     <section className='maxSection w-full lg:px-[2rem] px-[1rem] sm:py-[3rem] py-[1.5rem]'>
         <Alert big={isAlert.bigText} button1={isAlert.button} action1={()=>isAlert.action()} isON={isAlert.status} type={isAlert.type} message={isAlert.text} setON={(val)=>setAlert({...isAlert, status: false, text: ''})}/>
 
       <h2 className='uncut text-left 2xl:text-[1.8rem] xl:text-[1.6rem] md:text-[1.5rem] text-[1.5rem] font-semibold'>Product History</h2>
-      <div className='w-full py-[2rem] overflow-auto'>
-        
-                <table className='min-w-[500px] w-full uncut-regular'>
-                    <thead className='w-full xl:text-[1.1rem] text-[1rem] bg-[#EFEFEF]'>
-                        <tr className=' text-left'>
-                            <th className='pl-[1.5rem] py-[0.8rem]'>Name</th>
-                            <th className='py-[0.8rem]'>Variants</th>
-                            <th className='py-[0.8rem]'>Qty.</th>
-                            <th className='py-[0.8rem]'>Price</th>
-                            <th className='py-[0.8rem]'>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody className='bg-white'>
-                        {
-                            cartItems.map((item, index) => (
-                                <tr key={index} className='xl:text-[1rem] text-[0.9rem] bg-white border-b-2 transition-all hover:scale-[1.02]'>
-                                    <td className='py-[1rem] pl-[1.5rem]'>{item.product.productName}</td>
-                                    <td>
-                                        {item.product.variants.map((variant, index) => (
-                                            <div key={index} className='flex justify-start items-center gap-[1rem]'>
-                                                <small>{variant.name}</small>
-                                                <small>(GHS {variant.price})</small>
-                                            </div>
-                                        ))}
+      {cartItems.length > 0 ? (<>
+        <div className='w-full py-[2rem] overflow-auto'>
+                    <table className='min-w-[500px] w-full uncut-regular'>
+                        <thead className='w-full xl:text-[1.1rem] text-[1rem] bg-[#EFEFEF]'>
+                            <tr className=' text-left'>
+                                <th className='pl-[1.5rem] py-[0.8rem]'>Name</th>
+                                <th className='py-[0.8rem]'>Variants</th>
+                                <th className='py-[0.8rem]'>Qty.</th>
+                                <th className='py-[0.8rem]'>Price</th>
+                                <th className='py-[0.8rem]'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className='bg-white'>
+                            {
+                                cartItems.map((item, index) => (
+                                    <tr key={index} className='xl:text-[1rem] text-[0.9rem] bg-white border-b-2 transition-all hover:scale-[1.02]'>
+                                        <td className='py-[1rem] pl-[1.5rem]'>{item.product.productName}</td>
+                                        <td>
+                                            {item.product.variants.map((variant, index) => (
+                                                <div key={index} className='flex justify-start items-center gap-[1rem]'>
+                                                    <small>{variant.name}</small>
+                                                    <small>(GHS {variant.price})</small>
+                                                </div>
+                                            ))}
+                                            </td>
+                                        <td>{item.quantity}</td>
+                                        <td>GHS {item.totalPrice}</td>
+                                        <td className='text-center'>
+                                            <svg onClick={() => handleRemoveItem(item.product.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="lg:size-5 size-4 text-[#F5222D] cursor-pointer">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
                                         </td>
-                                    <td>{item.quantity}</td>
-                                    <td>GHS {item.totalPrice}</td>
-                                    <td className='text-center'>
-                                        <svg onClick={() => handleRemoveItem(item.product.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="lg:size-5 size-4 text-[#F5222D] cursor-pointer">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            
-        <div className='w-full py-[2rem] bg-white'>
-            
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                
+            <div className='w-full py-[2rem] px-[2rem] bg-white'>
+                {/* <Button  title="Export CSV" /> */}
+                <CSVLink data={cartItems} className='px-[1rem] py-[0.5rem] bg-[#0DD983] text-white '>Export</CSVLink>
+            </div>
         </div>
-      </div>
+      </>) : (<>
+        <div className='w-full flex flex-col justify-center items-center pt-[4rem]'><Empty text={'No history available'}/></div>
+      </>)}
     </section>
   )
 }
